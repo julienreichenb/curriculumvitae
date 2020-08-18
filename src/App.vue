640f776eec5c7c9c77638f2009cafd65235900c5
Julien <julien.reichenbach@students.hevs.ch>
2020-08-18 23:26:53 +0200

    github deploy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome on my CV"/>
+    <HelloWorld msg="Welcome on my new CV"/>
   </div>
 </template>
 
