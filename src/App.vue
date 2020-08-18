030de0053ca76e76d805af349740d2e988013a20
Julien <julien.reichenbach@students.hevs.ch>
2020-08-18 23:20:21 +0200

    github deploy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome on my CV"/>
   </div>
 </template>
 
@@ -17,8 +17,10 @@ export default {
 </script>
 
 <style>
+@import url(https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap);
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
