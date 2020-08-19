d2a377f8977b6064d07e5fa7704f0939b134fd15
Julien <julien.reichenbach@students.hevs.ch>
2020-08-19 20:30:25 +0200

    Fontawesome setup

--- src/components/menu/Navbar.vue
+++ src/components/menu/Navbar.vue
@@ -1,23 +1,16 @@
 <template>
-    <b-navbar id="navbar" toggleable="sm" sticky>
-        <b-navbar-toggle target="nav-text-collapse" />
-        <b-navbar-brand id="navbar-brand">
-            <img id="me" :src="pic" width="70" />
-            BootstrapVue
-        </b-navbar-brand>
-        <b-collapse id="nav-text-collapse" is-nav>
-            <b-navbar-nav>
-                <b-nav-text>Navbar text</b-nav-text>
-            </b-navbar-nav>
-        </b-collapse>
-  </b-navbar>
+    <div id="navbar" class="bg-dark">
+        <img id="me" :src="pic" width="200" />
+        <h1 class="display-4 text-info m-5 border-bottom border-light">JULIEN REICHENBACH</h1>
+        <font-awesome-icon :icon="['fad', 'acorn']" />
+    </div>
 </template>
 
 <script>
 import Pic from '@/assets/img/me.png'
 export default {
     props: {
-        items: {required: true}
+        items: { required: true }
     },
     data() {
         return {
@@ -32,17 +25,11 @@ export default {
 
 <style lang="scss" scoped>
 #navbar {
-    background-color: $base-color-dark;
-    border-bottom: 1px solid $base-color-darkest;
-
-    #navbar-brand {
-        color: white;
-    }
+    padding: 2rem 10rem;
 
     img#me {
-    border: 1px solid white;
-    border-radius: 15px;
-    filter: grayscale(.8)
+        border-radius: 55px;
+        filter: grayscale(.8)
     }
 
     img#me:hover {
--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ import VueMeta from "vue-meta";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+/* FontAwesome Config */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  FontAwesomeIcon,
+  FontAwesomeLayers,
+  FontAwesomeLayersText,
+} from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/pro-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fad } from '@fortawesome/pro-duotone-svg-icons'
+import { fal } from '@fortawesome/pro-light-svg-icons'
+
+library.add(fas)
+library.add(fab)
+library.add(far)
+library.add(fad)
+library.add(fal)
+
+// Register the component globally
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.config.productionTip = false
 
